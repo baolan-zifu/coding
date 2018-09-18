f2641e00619ab5527485e7385f1643c169d35b53
深蓝之邃 <maoyingyu00@gmail.com>
2018-09-18 03:00:35 +0000

    Update 1.c
    啊啊啊bug在哪里啊啊啊

--- 1.c
+++ 1.c
@@ -1,4 +1,3 @@
-
 int main()
 { int sex;
  int age;
@@ -6,16 +5,20 @@ printf( "hello Internet Of Things \n");
 printf( "我他喵的不会盲打啊啊啊 \n");
 printf("好无聊的编程课啊...");
  
+
  printf("please enter your gender.man 1    feman 2");
  scanf("%d",&sex);
  if (sex==1)
-  {printf("你的年龄是");
+     {
+  printf("你的年龄是");
    scanf("%d",&age);
-   printf("你真是小萝莉、n");
-  }
+   printf("你真是小萝莉\n");
   printf("你真可爱\n");
+  }
+ 
  else
   printf("cool~~~~~~\n");
 printf ("let's make it this fucking damn C艹");
+
 }
 
